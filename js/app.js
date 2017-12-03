a12de9bda2b9c3b28f56c69f38d592e5439779c3
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-12-03 12:50:52 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var Render = require('react-dom').render;
+var ProductData = require('./ProductData');
+var CartAPI = require('./utils/CartAPI');
+var FluxCartApp = require('./components/FluxCartApp.react.js');
+var createReactClass = require('create-react-class');
+
+// Load Mock Product Data into localStorage
+ProductData.init();
+
+// Load Mock API Call
+CartAPI.getProductData();
+
+// Render FluxCartApp Controller View
+Render(
+  <FluxCartApp />,
+  document.getElementById('flux-cart')
+);
